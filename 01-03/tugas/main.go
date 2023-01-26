abfe36eaa1d12704f5cf3ead82710aeea7ccbd7d
Zaenal Muttaqin <zaenalmuttaqin@zaen.taqin>
2023-01-27 06:46:37 +0700

    Update

--- 01-03/tugas/main.go
+++ 01-03/tugas/main.go
@@ -2,22 +2,22 @@ package main
 
 import "fmt"
 
+func reduceArray(n []int, k int) int {
+	for i := 0; i < len(n)-1; i++ {
+		for j := i + 1; j < len(n); j++ {
+			if n[i]*n[j] <= k {
+				n[i] *= n[j]
+				n = append(n[:j], n[j+1:]...)
+				fmt.Println("Iterasi-", i+1, n)
+				j--
+			}
+		}
+	}
+	return len(n)
+}
+
 func main() {
 	n := []int{1, 2, 4, 7, 1}
 	k := 9
-
-	for i := 1; len(n) > 0; i++ {
-		fmt.Printf("Iterasi-%d\n", i)
-		var newSlice []int
-		for j := 0; j < len(n); j++ {
-			if n[j] <= k {
-				k -= n[j]
-				newSlice = append(newSlice, n[j])
-			}
-		}
-		fmt.Println(newSlice)
-		n = newSlice
-	}
-
-	fmt.Println("Hasil:", len(n))
+	fmt.Println("Hasil:", reduceArray(n, k))
 }
