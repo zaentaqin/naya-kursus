ef93a16b0e045d79614aa94c72f06349696ab472
Zaenal Muttaqin <zaenalmuttaqin@zaen.taqin>
2023-02-01 14:32:57 +0700

    Update

--- 03-01/tugas/twotoone.go
+++ 03-01/tugas/twotoone.go
@@ -2,20 +2,22 @@ package main
 
 import (
 	"fmt"
-	"strings"
+	"runtime"
 )
 
-func TwoToOne(a string, b string) string {
+/* func TwoToOne(a string, b string) string {
 	var c []string
 	str := a + b
-	for r := 'z'; r >= 'a'; r-- {
+	for r := 'a'; r <= 'z'; r++ {
 		if strings.ContainsRune(str, r) {
 			c = append(c, string(r))
+			fmt.Println(c)
 		}
 	}
-	return strings.Join(c, " ")
-}
+	return strings.Join(c, "")
+} */
 
 func main() {
-	fmt.Println(TwoToOne("qwertyuioplkjh", "zxmjkqwdfhiow"))
+	//fmt.Println(TwoToOne("qAwertyuioplkjh", "zaxmjkqwdfhiow"))
+	fmt.Println(runtime.NumGoroutine())
 }
