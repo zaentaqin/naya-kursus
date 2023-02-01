6fe39f9b91807fdf80707c7e4d93029391cdcc87
Zaenal Muttaqin <zaenalmuttaqin@zaen.taqin>
2023-02-01 09:49:04 +0700

    Update

--- 03-01/latihan/oddnumber.go
+++ 03-01/latihan/oddnumber.go
@@ -3,9 +3,12 @@ package main
 import "fmt"
 
 func oddnumber(num int) int {
+	if num%2 == 1 {
+		return (num + 1) / 2
+	}
 	return num / 2
 }
 
 func main() {
-	fmt.Println(oddnumber(10))
+	fmt.Println(oddnumber(3))
 }
